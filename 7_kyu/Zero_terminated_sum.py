038524a270983f97b1eb19ae52104fef854ab797
Calvin Chan <calvin.chan@accedo.tv>
2020-07-06 23:18:32 +0200

    July 6 Kata Challenges Solutions
    July 6 Kata Challenges Solutions

---
+++ 7_kyu/Zero_terminated_sum.py
@@ -0,0 +1,28 @@
+# Kata: Zero Terminated Sum
+# https://www.codewars.com/kata/5a2d70a6f28b821ab4000004
+
+# This kata is part of the collection Mary's Puzzle Books.
+
+# Zero Terminated Sum
+
+# Mary has another puzzle book, and it's up to you to help her out! 
+# This book is filled with zero-terminated substrings, 
+# and you have to find the substring with the largest sum of its digits. 
+# For example, one puzzle looks like this:
+
+# "72102450111111090"
+# Here, there are 4 different substrings: 721, 245, 111111, and 9. 
+# The sums of their digits are 10, 11, 6, and 9 respectively. 
+# Therefore, the substring with the largest sum of its digits is 245, and its sum is 11.
+
+# Write a function largest_sum which takes a string and returns the maximum of the sums of the substrings. 
+# In the example above, your function should return 11.
+
+# Notes:
+
+# A substring can have length 0. For example, 123004560 has three substrings, 
+# and the middle one has length 0.
+# All inputs will be valid strings of digits, and the last digit will always be 0.
+
+def largest_sum(s):
+    return max(sum(map(int, subStr)) for subStr in s.split('0'))
